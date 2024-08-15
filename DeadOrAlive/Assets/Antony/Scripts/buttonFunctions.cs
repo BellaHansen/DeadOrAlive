c8e14705130822055906481c8fcfc1f7ebbe1975
byrdlariq <libyrd@student.fullsail.edu>
2024-08-15 03:09:30 -0400

    Added options menu

--- DeadOrAlive/Assets/Antony/Scripts/buttonFunctions.cs
+++ DeadOrAlive/Assets/Antony/Scripts/buttonFunctions.cs
@@ -4,11 +4,54 @@ using UnityEngine;
 
 //Add unity engine scene management to load scenes
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
+using UnityEngine.Audio;
 public class buttonFunctions : MonoBehaviour
 {
-   //Create resume function
-   public void resume()
+    public AudioMixer aud;
+
+    Resolution[] resolutions;
+    public Dropdown resolutionDrop;
+    List<string> resOption = new List<string>();
+    //keep in game manager
+    public void setVolume(float vol)
+    {
+        aud.SetFloat("volume", vol);
+    }
+    public void setQuality(int qualIndex)
+    {
+        QualitySettings.SetQualityLevel(qualIndex);
+    }
+    public void setFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+    public void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDrop.ClearOptions();
+        int currentResIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            resOption.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width &&
+            resolutions[i].height == Screen.currentResolution.height)
+            {
+                i = currentResIndex;
+            }
+        }
+        resolutionDrop.AddOptions(resOption);
+        resolutionDrop.value = currentResIndex;
+        resolutionDrop.RefreshShownValue();
+    }
+    public void SetResolution(int curResIndex)
+    {
+        Resolution res = resolutions[curResIndex];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+    }
+    //Create resume function
+    public void resume()
     {
         //Call the stateUnpause() from game manager
         gameManager.instance.stateUnpause();
